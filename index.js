07b8ce42cc3cbf403bd2bc14a585257b2b295b6a
unknown <fermejorado1@gmail.com>
2023-04-19 14:23:40 -0600

    Added some files to .gitignore and a simple hello cmd

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const dotenv = require('dotenv');
+const { Client, Events, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require('discord.js');
+const { config } = require('dotenv');
 
-dotenv.config();
+const prefix = '>' ;
+
+config();
 
 // Create a new client instance
 const client = new Client({
@@ -13,11 +15,29 @@ const client = new Client({
 	],
 });
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.on('ready', () => {
+	console.log('Bot is online!');
+
+	client.user.setActivity('Go outside lol', { type: 'WATCHING' });
 });
 
-// Log in to Discord with your client's token
+client.on('messageCreate', (message) => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	// message array
+
+	const messageArray = message.content.split(' ');
+	const argument = messageArray.slice(1);
+	const cmd = messageArray[0];
+
+	// COMMANDS
+
+	if (command === 'hello') {
+		message.channel.send(`Hello! ${message.author}`);
+	}
+});
+
 client.login(process.env.TOKEN);
